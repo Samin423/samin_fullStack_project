cc95005040acea85ac93b5866ab1d71700311132
shoronikaTanzila <105534738+shoronikaTanzila@users.noreply.github.com>
2025-12-10 10:23:07 +0600

    mongo atlas connected

---
+++ src/index.js
@@ -0,0 +1,54 @@
+//require("dotenv").config({path: ".env"});
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+
+dotenv.config({ 
+    path: "./env" 
+});
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//one approch to connect to data base and start server
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect('${process.env.MONGO_URI}/${DB_NAME}')
+    app.on("ERROR",(error)=> {     // event listener for server error                 
+        console.log("Error: ",error);
+        throw error;
+    })   
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+
+  } catch (error) {
+    console.error("Database connection failed", error);
+    throw error;
+  }
+})();
+
+*/
